7328d0ac2ea770ef1cec8f2fc201ad166e0053b1
Ingrid <ingridcruzrodrigues@gmail.com>
2020-05-26 19:52:45 -0300

    Adiciona página de cursos

---
+++ views/curso.php
@@ -0,0 +1,108 @@
+    <!DOCTYPE html>
+    <html lang="pt">
+    <head>
+        <meta charset="utf-8">
+        <title>Clientes</title>
+        <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+        <script src="https://kit.fontawesome.com/0c3fd300b7.js" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Empresa de Contabilidade em Salvador com mais de 14 anos de experiência. Alinhamos confiança e inovação."> 
+    </head>
+
+    <?php include('../includes/header.inc'); 
+    include('../includes/medias.inc');
+    ?>
+    <body>
+
+        <section class="all-page">
+
+            <div class="top-open">
+                <div class="top-page">
+                    <div class="content-top">
+                        <h1>CURSOS E TREINAMENTOS </h1>
+                    </div>      
+                </div>
+            </div>
+
+            <div class="cases">
+                <div class="box-cases">
+                    <h2> Ministramos cursos Presenciais e EAD de temas relacionados a Contabilidade.</h2>
+                    <div class="border-cases"></div>
+
+                    <div class="row-case">
+
+                        
+
+                    <div class="col-case">
+                        <div class="case">
+                            <img src="../assets/img/curso-esocial.jpg" alt="Logo do Esocial">
+                            <div class="name-case">ESOCIAL</div>
+                            <p>
+                                Curso sobre E-Social organizado pela KA Serviços Contábeis e ministrado pela contadora Mirian Figueiredo.<br>
+
+                                O esocial não trata-se de uma mudança na legislação. É apenas uma nova forma de informar aos órgãos integrantes do projeto tudo que acontece nas relações trabalhistas e até nas relações sem vínculo. O contador interpreta a figura do empregador e deverá estar atendo a todas as regras e todos os prazos que são exigidos no esocial. 
+                                <br><br>
+                            </p>                            
+                                <a href="ww.bit.ly/cursoesocial" class="button-c"> Acesse aqui </a>
+                        </div>
+                    </div>
+
+                    <div class="col-case">
+                            <div class="case">
+                                <img src="../assets/img/curso-departamento-pessoal.jpg" alt="Imagem Referente ao Departamento Pessoal">
+                                <div class="name-case">DEPARTAMENTO PESSOAL</div>
+                                <p>
+                                   Se você se interessa pela área de DP, deve querer ser um profissional qualificado na área. Talvez até já tentou se aventurar, mas percebeu que é uma área com muitos detalhes e de grande responsabilidade, seja por lidar com salários de pessoas (isso é bem delicado) ou multas de órgãos governamentais. <br>
+
+Venha para o nosso Curso de Departamento Pessoal e aprenda: <br>
+
+• Princípios, Leis e Normas trabalhistas <br>
+• Conceitos que envolvem a rotinas de pessoal (férias, 13 salário, benefícios e etc..) <br>
+• Processo de admissão  <br>
+• Processo de demissão <br>
++ E muito mais <br> <br>
+                                   </p>
+                                
+                                <a href="https://credencial.imasters.com.br/departamento-pessoal/informacoes" class="button-c"> Acesse aqui </a>
+                                
+                            </div>
+                        </div>
+
+                     <div class="col-case">
+                        <div class="case">
+                            <img src="../assets/img/curso-legalizacao.jpg" alt="Imagem Referente ao Curso de Legalização ">
+                            <div class="name-case">LEGALIZAÇÃO DE EMPRESAS</div>
+                            <p>
+                               APRENDA: <br><br>
+
+                                • Natureza jurídica - Conceitos <br>
+                                • CNAE - (Classificação nacional de atividades econômicas)<br>
+                                • Elaboração do Contrato Social e suas alterações.<br>
+                                • Processo de abertura, alteração e extinção na Sedur, na Juceb, na Receita Federal, na Sefaz– BA e Sefaz SSA. <br>
+                                • TVL (termo de viabilidade e localização) <br>
+                                • Regin (Juceb)<br>
+                                + E muito mais
+                               <br>
+                    
+                                <br>
+                                
+                            </p><a href="https://credencial.imasters.com.br/abertura-de-empresas" class="button-c"> Acesse aqui </a>
+                        </div>
+                    </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+
+    </body>
+
+    <?php 
+
+    include ("../includes/advertising.inc");
+    include("../includes/footer.inc"); ?>
+    </html>
+
